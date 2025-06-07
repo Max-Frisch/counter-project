749e933dc7ac5d8b4a0c7f62f35b734b4edd646f
Max <48467432+Max-Frisch@users.noreply.github.com>
2025-06-07 20:47:54 +0700

    added Test class for Counter class

--- src/Counter.java
+++ src/Counter.java
@@ -4,5 +4,42 @@
  * @author Max
  */
 public class Counter {
+	
+	//instance variables
+	
+	/**
+	 * Stores internal count.
+	 */
+	int count;
+	
+	//methods
+	
+	/**
+	 * Increments internal count variable.
+	 * @return new incremented value of count
+	 */
+	public int increment() {
+		this.count++;
+		
+		return this.count;
+	}
+	
+	/**
+	 * Decrements internal count variable.
+	 * @return new decremented value of count
+	 */
+	public int decrement() {
+		this.count--;
+		
+		return this.count;
+	}
+	
+	/**
+	 * Returns the current value of count
+	 * @return the count
+	 */
+	public int getCount() {
+		return this.count;
+	}
 
 }
